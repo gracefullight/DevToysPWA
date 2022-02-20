4f7ac088d369743f4ccbf505234518a52393d0b0
gracefullight <gracefullight.dev@gmail.com>
2022-02-21 00:27:48 +0900

    fix: 🐛 move theme to root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { NextUIProvider } from '@nextui-org/react';
 import { RecoilRoot } from 'recoil';
 import type { AppProps } from 'next/app';
 
-import { darkTheme } from './theme';
+import { darkTheme } from '../theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
